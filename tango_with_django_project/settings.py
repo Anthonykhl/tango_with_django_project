135ee1c6a76e8997e5dfed1287ac69758350797f
Anthony <2689407e@student.gla.ac.uk>
2023-01-23 14:43:51 +0000

    setting up temlates

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\antho\Workspace\tango_with_django_project\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
